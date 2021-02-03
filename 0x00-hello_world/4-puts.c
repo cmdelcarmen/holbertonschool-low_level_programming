8e5110fd8d9f545c96c60a3ef7466289b9ab32c4
cmdelcarmen <2720@holbertonschool.com>
2021-02-03 17:47:58 +0000

    update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+#include <string.h>
+
+/*This program prints out a sentence*/
+
+int main ()
+{	
+	char sentence[]="Programming is like building a multilingual puzzle";
+	puts(sentece);
+	return 0;
+}
