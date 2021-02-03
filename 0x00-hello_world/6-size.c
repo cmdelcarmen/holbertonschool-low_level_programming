ce9f93d33d9c3adf96e9ec095d41742b7253b42d
cmdelcarmen <2720@holbertonschool.com>
2021-02-03 19:00:41 +0000

    update

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - function prints the required text and returns 0
+* Return: 0
+*/
+
+int main(void)
+{   
+    char char_var;
+    int int_var;
+    long int long_int_var;
+    long long int long_long_int;
+    float float_var;
+
+    printf("Size of a char: %zu byte(s)\n", sizeof(char_var));
+    printf("Size of an int: %zu byte(s)\n", sizeof(int_var));
+    printf("Size of a long int: %zu byte(s)\n", sizeof(long_int_var));
+    printf("Size of a long long int: %zu byte(s)\n", sizeof(long_int_var));
+    printf("Size of a float: %zu byte(s)\n", sizeof(long_long_int));
+    
+    return (0);
+}
+
