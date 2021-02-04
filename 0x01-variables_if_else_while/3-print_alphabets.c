0821aaf1846c6e3fd4f1be7b0f2761e58cac7251
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-04 20:14:02 +0000

    update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 /**
-* main - Produces a random number, then prints a string
-* Return: 0
-*/
+ * main - Produces a random number, then prints a string
+ * Return: 0
+ */
 
 int main(void)
 {
 
-	char ch = 97;
+	char lowerCase = 97;
+	char upperCase = 65;
 
-	while (ch < 123)
+	while (lowerCase < 123)
 	{
-		putchar(ch);
-		if (ch !=(9));
-	 {
-		putchar(",");
-		putchar(" ");
-		}
-		ch++;
+		putchar(lowerCase);
+		lowerCase++;
+	}
+
+	while (upperCase < 91)
+	{
+		putchar(upperCase);
+		upperCase++;
 	}
 
 	putchar('\n');
