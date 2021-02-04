6cfaa6baec6663e862312f0f09b1e8fa88bcfaf0
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-04 20:47:17 +0000

    update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*  main - Produces a random number, then prints a string
+*  Return: 0
+*/
+
+int main(void)
+{
+
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+
+	putchar('\n');
+	return (0);
+}
