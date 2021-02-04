53b589992113bb7a7aab290ba76f788c6c87a511
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-04 20:39:50 +0000

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Produces a random number, then prints a string
+* Return: 0
+*/
+
+int main(void)
+{
+
+	char lowerCase = 97;
+
+	while (lowerCase < 123)
+	{
+		if (lowerCase != 113)
+		{
+			putchar(lowerCase);
+		}
+	lowerCase++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
