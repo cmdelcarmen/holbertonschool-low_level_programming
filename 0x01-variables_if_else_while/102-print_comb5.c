8235d445fcfbae2203ce07e6b9508b187f0eab54
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-05 02:54:17 +0000

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
 * main - function prints the required text and returns 0
 * Return: 0
 */
@@ -7,25 +7,24 @@ int main(void)
 {
 	int a;
 	int b;
-	int c;
-
-	for (a = '0'; a <= '9'; a++)
+	for (a = 0; a <= 99 ; a++)
 	{
-		for (b = a + 1; b <= '9'; b++)
+		for(b = a +1; b <= 99; b++)
 		{
-			for (c = b + 1; c <= '9'; c++)
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');	
+			putchar((b % 10) + '0');
+
+			if (a <= 97)
 			{
-				putchar(a);
-				putchar(b);
-				putchar(c);
-				if (a != '7')
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
 	putchar ('\n');
 	return (0);
 }
+
