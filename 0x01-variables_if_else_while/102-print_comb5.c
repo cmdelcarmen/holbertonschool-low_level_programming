d5a6ab2d8fd27bf54ab48860488d8cb3c672ae41
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-05 02:58:49 +0000

    commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,14 +7,15 @@ int main(void)
 {
 	int a;
 	int b;
+
 	for (a = 0; a <= 99 ; a++)
 	{
-		for(b = a +1; b <= 99; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
 			putchar((a / 10) + '0');
 			putchar((a % 10) + '0');
 			putchar(' ');
-			putchar((b / 10) + '0');	
+			putchar((b / 10) + '0');
 			putchar((b % 10) + '0');
 
 			if (a <= 97)
