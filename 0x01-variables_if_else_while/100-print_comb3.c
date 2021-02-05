a0698549e5bd51a088180f4f8822199acd238e11
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-05 00:59:50 +0000

    update

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main - Produces a random number, then prints a string
+* Return: 0
+*/
+
+#int main(void)
+{
+	int a;
+	int b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+			putchar(a);
+			putchar(b);
+			if (a != '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar ('\n');
+return (0);
+}
