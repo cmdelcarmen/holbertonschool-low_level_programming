e4e5d3979e549be397d44e6c4a564c0fb3f5f818
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-09 05:20:03 +0000

    update

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
-
-int main ()
+/**
+* main - funcation calculates sum on multiples
+*
+* Return: 0
+*/
+int main(void)
 {
 	int num = 3;
 	int multipleOfThree;
 	int multipleOfFive;
-	int sum =0;
+	int sum = 0;
 
-	while (num < 1024) {
+	while (num < 1024)
+	{
 
 		multipleOfThree = num % 3;
 		multipleOfFive = num % 5;
 
-		if (multipleOfThree == 0 || multipleOfFive == 0){
-
-			sum +=num;
+		if (multipleOfThree == 0 || multipleOfFive == 0)
+		{
+			sum += num;
 		}
 
 		num++;
 	}
 
-	printf("%i ", sum); 
+	printf("%i ", sum);
 	printf(" \n");
+	
+	return 0;
 }
