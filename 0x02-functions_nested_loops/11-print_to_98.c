f578b5bf0c8ef89bd6931be939ba9427a0d56cf4
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-09 05:49:23 +0000

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,26 +1,22 @@
+#include "holberton.h"
+#include <unistd.h>
 #include <stdio.h>
 /**
-* print_to_98 - check the code for Holberton School students.
+* print_to_98 - function print numbers up to 98
+* @n: the number being conted
 *
-* @n: the number being counted
-*/
+* Return: 0
+**/
 
 void print_to_98(int n)
 {
-
-	if (n < 99)
+	if (n <= 98)
 	{
-		for(n = n; n <= 99; n++)
-
-			printf("%i, ", n);
+		for (n = n; n <= 97; n++)
+			printf("%d, ", n);
 	}
-
 	else
-	{	for(n = n; n >= 98; n--)
-
-		printf("%i, ", n++);
-	}
-
-	printf("98 \n");
+		for (n = n; n > 98; n--)
+			printf("%d, ", n);
+	printf("98\n");
 }
-
