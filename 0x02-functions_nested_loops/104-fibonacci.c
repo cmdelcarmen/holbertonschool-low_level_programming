4e7e3fe44acb495406944062117102c444539b6e
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-09 07:35:07 +0000

    update

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+* main - funtion prints out first 50 fibonacci numbers
+*
+* Return: 0
+*/
+int main(void)
+{
+	int n = 0;
+	unsigned long int sum = 0;
+	unsigned long int prevSum1 = 0;
+	unsigned long int prevSum2 = 1;
+
+	while (n < 99)
+	{
+		sum = prevSum1 + prevSum2;
+		printf("%lu", sum);
+
+		prevSum1 = prevSum2;
+		prevSum2 = sum;
+
+		if (n != 49)
+		{
+			printf(", ");
+		}
+		else
+		{
+			printf("\n");
+		}
+		n++;
+	}
+	return (0);
+}
+
