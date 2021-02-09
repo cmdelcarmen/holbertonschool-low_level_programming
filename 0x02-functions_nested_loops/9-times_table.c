a75918a267a8dfbfea35a23c218867ab7b912987
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-09 08:39:19 +0000

    update

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+* times_table - function prints multiples of 9
+*
+* Return: 0
+*/
+
+void times_table(void)
+{
+	int num;
+	int multiple;
+	int nineTimes;
+
+
+	for (num = 0; num < 10; num++)
+
+	{
+		_putchar('0');
+
+		for (multiple = 1; multiple < 10; multiple++)
+		{
+			_putchar(44);
+			_putchar(32);
+
+			nineTimes = num * multiple;
+
+			if (nineTimes < 10)
+				_putchar(32);
+
+			else
+				_putchar((nineTimes / 10) + '0');
+
+			_putchar((nineTimes % 10) + '0');
+		}
+
+		_putchar('\n');
+	}
+}
