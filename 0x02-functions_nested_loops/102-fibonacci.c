7a806b7ae10d2fbb3ff25c269470772b5e3d7c7f
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-09 06:50:03 +0000

    update

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,31 +2,32 @@
 /**
 * main - funtion prints out first 50 fibonacci numbers
 *
-* Return : 0
+* Return: 0
 */
 int main(void)
 {
 	int n = 0;
-	int sum = 0;
+	unsigned long int sum = 0;
 	unsigned long int prevSum1 = 0;
 	unsigned long int prevSum2 = 1;
 
 	while (n < 50)
 	{
 		sum = prevSum1 + prevSum2;
-		printf("%i", sum);
+		printf("%lu", sum);
 
 		prevSum1 = prevSum2;
 		prevSum2 = sum;
 
-		if (n != 49)
+			if (n != 49)
 			{
 				printf(", ");
 			}
-		else
+				else
+			{
 				printf("\n");
+			}
 		n++;
-	}	
-
+	}
 	return (0);
 }
