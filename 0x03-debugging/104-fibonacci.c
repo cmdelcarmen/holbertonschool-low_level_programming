9e45a40902164b4cfd0ea874e8a1100c701203ff
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-10 02:18:29 +0000

    update

--- 0x03-debugging/104-fibonacci.c
+++ 0x03-debugging/104-fibonacci.c
@@ -9,14 +9,14 @@ int main(void)
 	double fib1 = 1, fib2 = 2, fib3, num, count = 0;
 
 	num = 99;
-	printf("%lf, ", fib1);
-	printf("%lf, ", fib2);
+	printf("%.0lf, ", fib1);
+	printf("%.0lf, ", fib2);
 	count = 4;
 	while (count < num)
 	{
 		fib3 = fib1 + fib2;
 		count++;
-		printf("%lf", fib3);
+		printf("%0.lf", fib3);
 		fib1 = fib2;
 		fib2 = fib3;
 		if (count != 99)
