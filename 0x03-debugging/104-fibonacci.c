a557a3eb2fbdcca6dfb0ca5b770376e22979462e
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-10 02:12:16 +0000

    update

---
+++ 0x03-debugging/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+*main - prints fibonacci numbers
+*
+* Return: 0
+*/
+int main(void)
+{
+	double fib1 = 1, fib2 = 2, fib3, num, count = 0;
+
+	num = 99;
+	printf("%lf, ", fib1);
+	printf("%lf, ", fib2);
+	count = 4;
+	while (count < num)
+	{
+		fib3 = fib1 + fib2;
+		count++;
+		printf("%lf", fib3);
+		fib1 = fib2;
+		fib2 = fib3;
+		if (count != 99)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
