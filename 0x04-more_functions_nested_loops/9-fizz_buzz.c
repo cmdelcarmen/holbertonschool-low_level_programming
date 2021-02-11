d077819fc1c4e7168eed155b2a8733678a2dd867
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-11 18:04:56 +0000

    update

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+* main - function prints underscores
+*
+* Return: 0
+*/
+int main(void)
+{
+
+	int number;
+
+	for (number = 1; number < 101; number++)
+	{
+		if (number % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+
+		else if (number % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+
+		else if (number % 3 == 0 && number % 5 == 0)
+		{
+			printf("Fizz Buzz ");
+		}
+
+		else
+			printf("%i ", number);
+	}
+	printf("\n");
+	return (0);
+}
