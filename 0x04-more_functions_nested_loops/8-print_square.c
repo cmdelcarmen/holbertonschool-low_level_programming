2c1ccbe4cdaf1ced506521b3d0a48891570a6086
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-11 17:36:20 +0000

    update

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,9 +9,9 @@ void print_square(int size)
 	int count;
 	int count3;
 
-	for (count3 = 1; count3 < size; count3++)
+	for (count3 = 0; count3 < size; count3++)
 	{
-		for (count = 1; count < size; count++)
+		for (count = 0; count < size; count++)
 		{
 			_putchar(35);
 		}
