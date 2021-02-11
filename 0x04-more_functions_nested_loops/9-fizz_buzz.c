8b292060077342ca059c9ea517e4f700136bc627
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-11 18:20:00 +0000

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,19 +11,19 @@ int main(void)
 
 	for (number = 1; number < 101; number++)
 	{
-		if ((number % 3 == 0) && (number %5 == 0 ))
+		if ((number % 3 == 0) && (number % 5 != 0))
 		{
-			printf("FizzBuzz ");
+			printf("Fizz ");
 		}
 
-		else if (number % 5 == 0)
+		else if ((number % 5 == 0) && (number % 3 != 0))
 		{
 			printf("Buzz ");
 		}
 
-		else if (number % 3 == 0)
+		else if ((number % 3 == 0) && (number % 5 == 0))
 		{
-			printf("Fizz ");
+			printf("FizzBuzz ");
 		}
 
 		else
