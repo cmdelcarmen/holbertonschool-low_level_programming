15b85c89fcda245efddc62f91e25c5aac7ad74f0
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-12 02:03:31 +0000

    update

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+* print_triangle - prints a square character
+* @big: how big the square is
+*/
+
+void print_triangle(int big)
+{
+	int rows;
+	int hashes;
+	
+	if (big <= 0)
+		_putchar('\n');
+	
+	for (rows = 1; rows <= big; rows++)
+	{
+		for (hashes = 1; hashes <= big; hashes++)
+		{
+			if (hashes <= (big - rows))
+				_putchar(' ');
+
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
