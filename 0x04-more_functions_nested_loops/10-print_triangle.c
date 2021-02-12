3f43deb6e70424cd792b553f859f025a8463b7d8
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-12 02:09:00 +0000

    update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,10 +8,10 @@ void print_triangle(int big)
 {
 	int rows;
 	int hashes;
-	
+
 	if (big <= 0)
 		_putchar('\n');
-	
+
 	for (rows = 1; rows <= big; rows++)
 	{
 		for (hashes = 1; hashes <= big; hashes++)
