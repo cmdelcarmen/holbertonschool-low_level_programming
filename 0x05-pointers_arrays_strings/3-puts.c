5f2c96dc949ff6912d8afb6ff1f08663bd9e6357
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-15 04:35:21 +0000

    update

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,13 @@
 #include "holberton.h"
-
+/**
+* _puts - function displays string
+* @str: pointer passed to function
+*/
 void _puts(char *str)
 
 {
 	while (*str != '\0')
-	{	
+	{
 		_putchar(*str);
 		str++;
 	}
