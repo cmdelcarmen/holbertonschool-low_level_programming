e02d0b7d03cb61a324b187b3c75b8a8d30171440
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-15 06:04:49 +0000

    update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,26 +1,26 @@
 #include "holberton.h"
 /**
-* puts2 - function puts2
-* @str: pointer passed
-*/
+ * puts2 - function puts2
+ * @str: pointer passed
+ */
 void puts2(char *str)
 {
 	int arrayLength, count;
 
 	for (count = 0; str[count] != '\0'; count++)
-		{
-			arrayLength++;
-		}
+	{
+		arrayLength++;
+	}
 
 	arrayLength--;
 
 	for (count = 0; count <= arrayLength; count++)
+	{
+		if (count % 2 == 0)
 		{
-				if (count % 2 == 0)
-				{
-					_putchar(*str);
-				}
-			str++;
+			_putchar(*str);
 		}
+		str++;
+	}
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,8 @@
 #include "holberton.h"
-
+/**
+ * puts_half - functio prints out half a string
+ * @str: pointer passed
+ */
 void puts_half(char *str)
 {
 	int count, arrayLength, printLength;
@@ -7,17 +10,17 @@ void puts_half(char *str)
 	arrayLength = 0;
 	printLength = 0;
 
-	for(count = 0; str[count] != '\0'; count++)
-		{
-			arrayLength++;
-		}
+	for (count = 0; str[count] != '\0'; count++)
+	{
+		arrayLength++;
+	}
 
 	printLength = (arrayLength / 2);
 
-	for(count = printLength; count <= arrayLength; count++)
-		{
-			_putchar(str[count]);
-		}
+	for (count = printLength; count <= arrayLength; count++)
+	{
+		_putchar(str[count]);
+	}
 	_putchar('\n');
 }
 
