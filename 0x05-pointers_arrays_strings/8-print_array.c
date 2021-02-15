47d7d6ea0e0d20f4efe814aaa7acfa8db4645a08
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-15 05:28:50 +0000

    update

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,18 +1,22 @@
 #include "holberton.h"
 #include <stdio.h>
-
+/**
+* print_array - function prints out n elements of array
+* @a: pointer passed to function
+* @n: pointer passed to function
+*/
 void print_array(int *a, int n)
 {
 	int count;
 
 	for (count = 0; count <= (n - 1); count++)
+	{
+		if (count != (n - 1))
 		{
-			if (count != (n - 1))
-				{
-					printf("%i, ", a[count]);
-				}
-
-			else
-				printf("%i\n", a[count]);
+			printf("%i, ", a[count]);
 		}
+
+		else
+			printf("%i\n", a[count]);
+	}
 }
