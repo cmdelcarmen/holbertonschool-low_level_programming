730c4ec4b158d7aa5f9bb5d77241d9c5b5a6d40a
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-18 20:35:00 +0000

    update

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - check the code for Holberton School students.
+* reverse_array - check the code for Holberton School students.
 * @a: an array of integers
 * @n: the number of elements to swap
 *
