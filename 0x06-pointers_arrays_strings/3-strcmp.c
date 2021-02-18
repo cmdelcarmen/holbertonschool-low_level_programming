400a3d547f8cf472a6ef8b23333a5ccc501ccb2c
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-18 19:14:03 +0000

    update

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+* _strcmp - function compares two strings
+* @s1: pointer
+* @s2: pointer
+*
+* Return: Always 0.
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int sum;
+
+	return (sum = *s1 - *s2);
+}
