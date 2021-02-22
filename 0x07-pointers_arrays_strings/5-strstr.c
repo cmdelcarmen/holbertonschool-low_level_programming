36cbd8a1df38731476eea1a92d9f43468a487b5f
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-22 20:18:15 +0000

    update

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * *_strstr - check the code for Holberton School students.
+ * @haystack: pointer passed
+ * @needle: pointer passed
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int count, count2;
+	int match = 0;
+
+	for (count = 0; haystack[count] != '\0'; count++)
+	{
+			for (count2 = 0; haystack[count] == needle[count2]; count2++)
+			{
+					if (haystack[count + count2] == needle[count2])
+						match = 1;
+					else
+						match = 0;
+			}
+			if (match == 1)
+				return (haystack += count);
+	}
+	return ('\0');
+}
