2626fab0253af5f83a423549d747e0ad481dc9e7
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-22 07:52:29 +0000

    update

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * *_strpbrk - check the code for Holberton School students.
+ * @s: array passed
+ * @accept: array passed
+ *
+ * Return: s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int count, count2;
+	int printPoint = 0;
+	int stop = 1;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		for (count2 = 0; accept[count2] != '\0'; count2++)
+		{
+			if (stop == 1)
+			{
+				if (s[count] == accept[count2])
+				{
+					printPoint = count;
+					stop = 0;
+				}
+			}
+		}
+	}
+
+	s += printPoint;
+
+	return (s);
+}
