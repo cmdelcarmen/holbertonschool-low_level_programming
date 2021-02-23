2c1628b01b5bd4ba8b99e56ef973c7359781a7f6
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-23 03:26:47 +0000

    update

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -2,7 +2,8 @@
 
 /**
  * set_string - check the code for Holberton School students.
- *
+ * @s: pointer to pointer
+ * @to: pointer passed
  * Return: Always 0.
  */
 void set_string(char **s, char *to)
