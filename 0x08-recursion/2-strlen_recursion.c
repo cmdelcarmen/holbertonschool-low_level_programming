3049bb9d8e2ecabc42a4c3012f4c8f75211f2819
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-24 23:41:25 +0000

    update

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,13 +9,13 @@
 int _strlen_recursion(char *s)
 {
 
-	if ( s[0] == '\0')
+	if (s[0] == '\0')
 	{
 		return (0);
 	}
 	else
-	{	
+	{
 		s++;
-		return 1 + _strlen_recursion(s);
+		return (1 + _strlen_recursion(s));
 	}
 }
