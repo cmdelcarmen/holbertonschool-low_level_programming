f4b2c93029c14115118167198ac5a166760f9106
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-24 23:39:24 +0000

    update

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - check the code for Holberton School students.
+ * @s: pointer passed
+ * Return: count
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	if ( s[0] == '\0')
+	{
+		return (0);
+	}
+	else
+	{	
+		s++;
+		return 1 + _strlen_recursion(s);
+	}
+}
