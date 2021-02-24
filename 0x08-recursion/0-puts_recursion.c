80d1e2727f4f376ac82becb831b5b729b38cb269
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-24 15:18:46 +0000

    update

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - check the code for Holberton School students.
+ * @s: pointer passed
+ */
+void _puts_recursion(char *s)
+{
+	{
+		if (*s != '\0')
+		{
+			_putchar(*s);
+			s++;
+			_puts_recursion(s);
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+}
