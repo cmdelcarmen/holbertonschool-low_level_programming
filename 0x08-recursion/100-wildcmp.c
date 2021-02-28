34942487dd9ff2a4dde11b0a26f0ba13c607eb67
cmdelcarmen <delcarmencaroline@gmail.com>
2021-02-28 07:09:25 +0000

    update

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
-int getCompare(char *s1, char *s2, int same, int arrayLength1, int arrayLength2);
+int getCompare(char *s1, char *s2, int same,
+		int arrayLength1, int arrayLength2);
 char movePointer(char *s);
 int getLength(char *s, int arrayLength);
 
+/**
+ * wildcmp - check the code for Holberton School students.
+ * @s1: array passed
+ * @s2:array passed
+ * Return: same
+ */
 int wildcmp(char *s1, char *s2)
 {
-	int same = 0, arrayLength1 = 0, arrayLength2 =0;
+	int same = 1, arrayLength1 = 0, arrayLength2 = 0;
 
 	arrayLength1 = getLength(s1, arrayLength1);
 	arrayLength2 = getLength(s2, arrayLength2);
@@ -15,6 +22,12 @@ int wildcmp(char *s1, char *s2)
 	return (same);
 }
 
+/**
+ * getLength- check the code for Holberton School students.
+ * @s: array passed
+ * @arrayLength: length of array
+ * Return: length of array
+ */
 int getLength(char *s, int arrayLength)
 {
 	if (s[arrayLength] != '\0')
@@ -29,9 +42,19 @@ int getLength(char *s, int arrayLength)
 	return (getLength(s, arrayLength));
 }
 
-int getCompare(char *s1, char *s2, int same, int arrayLength1, int arrayLength2)
+/**
+ * getCompare - check the code for Holberton School students.
+ * @s1: array passed
+ * @s2: array passed
+ * @arrayLength1: length of array
+ * @arrayLength2: length of array
+ * @same: bool variable
+ * Return: length of array
+ */
+int getCompare(char *s1, char *s2, int same,
+		int arrayLength1, int arrayLength2)
 {
-	if ((s2[arrayLength2] != '*') && (s1[arrayLength1] != '\0') 
+	if ((s2[arrayLength2] != '*') && (s1[arrayLength1] != '\0')
 			&& (s2[arrayLength2] != '\0'))
 	{
 		if (s1[arrayLength1] == s2[arrayLength2])
@@ -58,5 +81,5 @@ int getCompare(char *s1, char *s2, int same, int arrayLength1, int arrayLength2)
 		arrayLength1 -= 1;
 	}
 
-	return(getCompare(s1, s2, same, arrayLength1, arrayLength2));
+	return (getCompare(s1, s2, same, arrayLength1, arrayLength2));
 }
