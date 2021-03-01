2a2b1d1b76ceecac5baf3def324b49d607dd2494
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-01 00:06:45 +0000

    update

---
+++ 0x09-static_libraries/CFiles/_isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * * _isupper - function evaluates wheter c is a number
+ * * @c: number valued
+ * *
+ * * Return: returns 1 if digit, 0 if otherwise
+ * **/
+
+int _isupper(int c)
+{
+
+		if (c > 64 && c < 91)
+
+					return (1);
+
+			else
+						return (0);
+}
