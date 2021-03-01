64c753c46a2a44254c674f67d41749ab5a1a3bc3
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-01 22:09:35 +0000

    update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,65 +2,50 @@
 #include <stdlib.h>
 /**
  * main - prints minimum number of coins to make change for an amount of money
- * for example: if input 10 - output 1 bc you only need 1 dime, if input is 100 then output
- * is 4 because u only need 4 quaters. if input is 13, then output is 3 bc u need
  * @argc: parameter
  * @argv: parameter
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int twentyFiveVal = 25, tenVal = 10, fiveVal = 5, twoVal = 2, oneVal = 1;
-	int count = 0, count2, cents, print = 0;
+	int ten = 10, five = 5, two = 2, oneVal = 1, count = 0, num, cents;
 
 	cents = atoi(argv[1]);
-
 	if (argc != 2)
 	{
-		printf("Error\n");
-		return (1);
+		return (printf("Error\n"));
 	}
-	
-	for (count2 = 0; cents > 0; count2++)
+	for (num = 0; cents > 0; num++)
 	{
-		print = 1;
-
 		if (cents >= 25)
 		{
-			cents -= twentyFiveVal;
+			cents -= 25;
 			count++;
 			continue;
 		}
 		else if ((cents >= 10) && (cents < 25))
 		{
-			cents -= tenVal;
+			cents -= ten;
 			count++;
 			continue;
 		}
-		else if((cents >= 5) && (cents < 10))
+		else if ((cents >= 5) && (cents < 10))
 		{
-			cents -= fiveVal;
+			cents -= five;
 			count++;
 			continue;
 		}
 		else if ((cents >= 2) && (cents < 5))
-		{
-			cents -= twoVal;
+		{   cents -= two;
 			count++;
 			continue;
 		}
 		else if (cents == 1)
-		{
-			cents -= oneVal;
+		{	cents -= oneVal;
 			count++;
 			continue;
 		}
 	}
-
-	if (print == 1 )
-	{
-		printf("%d \n", count);
-	}
-
+	printf("%d \n", count);
 	return (0);
 }
