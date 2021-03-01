e5ad709969d85db48074360845afde5e06a41292
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-01 07:03:01 +0000

    update

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints minimum number of coins to make change for an amount of money
+ * for example: if input 10 - output 1 bc you only need 1 dime, if input is 100 then output
+ * is 4 because u only need 4 quaters. if input is 13, then output is 3 bc u need
+ * @argc: parameter
+ * @argv: parameter
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	
+
+	return (0);
+}
