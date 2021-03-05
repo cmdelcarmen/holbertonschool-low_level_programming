21b8a4f16be1a3bb1314f0073351043b76fd3f8c
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-05 04:24:04 +0000

    update

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program multiplies two potive numbers
+ * @argc: parameter
+ * @argv: parameter
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	long int product, count, count2, print = 1;
+
+	if (argc == 3)
+	{
+		for (count = 1; count < argc; count++)
+		{
+			for (count2 = 0; argv[count][count2] != '\0'; count2++)
+			{
+				if (argv[count][count2] < '0' || argv[count][count2] > '9')
+				{
+					print = 0;
+					break;
+				}
+			}
+		product = (atoi(argv[1]) * atoi(argv[2]));
+		}
+	}
+	else
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (print == 1)
+	{
+		printf("%ld\n", product);
+	}
+	if (print == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	return (0);
+}
