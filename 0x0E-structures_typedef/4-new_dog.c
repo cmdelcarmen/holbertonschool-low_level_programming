ea8860dce2238fe9549a27fc289bd3433ad20836
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-09 03:25:29 +0000

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -32,7 +32,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	(*newDog).owner = malloc((sizeof(char) * arrayLength2) + 1);
 		if ((*newDog).owner == NULL)
 		{
-			free((*newDog).owner);
+			free((*newDog).name);
 			free(newDog);
 			return (NULL);
 		};
