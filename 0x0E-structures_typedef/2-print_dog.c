51ca8c912738bcd9ab667b70090bbc4da99174b4
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-09 01:29:15 +0000

    update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,31 +14,31 @@ void print_dog(struct dog *d)
 
 	else
 	{
-		if ((*d).name != 0)
-		{
-			printf("Name: %s\n", (*d).name);
-		}
-		else
+		if ((*d).name == NULL)
 		{
 			printf("Name: (nil)\n");
 		}
-
-		if ((*d).age != 0)
-		{
-			printf("Age: %f\n", (*d).age);
-		}
 		else
+		{
+			printf("Name: %s\n", (*d).name);
+		}
+
+		if ((*d).age < 0)
 		{
 			printf("Age: (nil)\n");
 		}
-
-		if ((*d).owner != 0)
+		else
 		{
-			printf("Owner: %s\n", (*d).owner);
+			printf("Age: %f\n", (*d).age);
+		}
+
+		if ((*d).owner == NULL)
+		{
+			printf("Owner: (nil)\n");
 		}
 		else
 		{
-			printf("Owner: (nil)\n");
+			printf("Owner: %s\n", (*d).owner);
 		}
 	}
 }
