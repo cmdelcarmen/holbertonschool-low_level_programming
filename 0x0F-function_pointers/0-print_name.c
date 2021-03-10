3fb1b132dc1368b58824b70900a04178158e65e4
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-10 19:53:32 +0000

    update

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * print_name - funtion prints a name
+ * @name: array of chars passed
+ * @f: function passed
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+}
