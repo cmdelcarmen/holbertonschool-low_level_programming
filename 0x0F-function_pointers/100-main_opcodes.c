f89a0041700f98a7fd117ee071c7d1c166087ad0
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-11 03:26:45 +0000

    update

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (argv[1][0] == '-')
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	return (0);
+}
