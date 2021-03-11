56ca5266b28b8de074a0c42bf2b635de4045bf7b
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-11 00:26:11 +0000

    update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,6 +18,11 @@ int main(int argc, char **argv)
 		exit(98);
 	}
 
+	if ((*argv[2] == '/' || *argv[2] == '%') && (*argv[3] == '0'))
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
