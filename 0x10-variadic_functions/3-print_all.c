66adefbc57cc7cc94ff41d664930435f66be739e
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-11 22:21:16 +0000

    update

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,65 +2,50 @@
 #include <stdio.h>
 
 /**
- * getLength - function
- * @s: array of char
- * Return: int:
- */
-int getLength(const char * const s)
-{
-	int arrayLength = 0;
-
-	while (s[arrayLength] != '\0')
-	{
-		arrayLength++;
-	}
-	return (arrayLength);
-}
-
-/**
  * print_all - function prints anything
  * @format: data types
  * Return: nothing:
  */
 void print_all(const char * const format, ...)
 {
-	int count2 = 0, comma = 0, stop;
+	int count2 = 0;
 	va_list printList;
-	char *string;
+	char *string, *comma = "";
+
+	if (format == NULL)
+	{
+		printf("\n");
+		return;
+	}
 
 	va_start(printList, format);
-	stop = getLength(format);
-	while (format != NULL && format[count2] != '\0')
+	while (format[count2] != '\0')
 	{
 		switch (format[count2])
 		{
 			case 'c':
-				printf("%c", va_arg(printList, int));
-				comma = 1;
+				printf("%s%c", comma, va_arg(printList, int));
 				break;
+
 			case 'i':
-				printf("%i", va_arg(printList, int));
-				comma = 1;
+				printf("%s%i", comma, va_arg(printList, int));
 				break;
+
 			case 'f':
-				printf("%f", va_arg(printList, double));
-				comma = 1;
+				printf("%s%f", comma, va_arg(printList, double));
 				break;
+
 			case 's':
 				string = va_arg(printList, char *);
 				if (string == NULL)
 					string = "(nil)";
-				printf("%s", string);
-				comma = 1;
+				printf("%s%s", comma, string);
 				break;
 		}
+		comma = ", ";
 		count2++;
-		if ((comma == 1) && count2 < stop)
-		{
-			printf(", ");
-			comma = 0;
-		}
 	}
+
 	printf("\n");
 	va_end(printList);
 }
