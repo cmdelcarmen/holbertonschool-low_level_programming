b67d748c8e41ce5bb5298b52e9736d3cc5963ae2
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-18 23:08:08 +0000

    vanilla ice

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,34 +1,20 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- *  * main - check the code for Holberton School students.
- *   *
- *    * Return: Always 0.
- *     */
-int main(void)
-{
-	list_t *head;
-	list_t *new;
-	list_t hello = {"World", 5, NULL};
-	size_t n;
+ * list_len - function returns the number of elements in a linked list_t list
+ * @h: head to linked list
+ * Return: number of elements in a linked list
+ */
 
-	head = &hello;
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+size_t list_len(const list_t *h)
+{
+	int numOfMods = 0;
+
+	while (h != NULL)
 	{
-		printf("Error\n");
-		return (1);
+		h = h->next;
+		numOfMods++;
+
 	}
-	new->str = strdup("Hello");
-	new->len = 5;
-	new->next = head;
-	head = new;
-	n = list_len(head);
-	printf("-> %lu elements\n", n);
-	free(new->str);
-	free(new);
-	return (0);
+	return (numOfMods);
 }
