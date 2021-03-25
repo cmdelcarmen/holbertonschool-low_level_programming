ea5ffddcf9e917e63356f39da6e48d8a0f5ef51c
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-25 20:46:36 +0000

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -19,7 +19,7 @@ int get_bit(unsigned long int n, unsigned int index)
 		return (0);
 	}
 
-	if (index > sizeof(unsigned long int))
+	if (index > sizeof(unsigned long int) * 8)
 	{
 		return (-1);
 	}
