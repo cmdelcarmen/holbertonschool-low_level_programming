e7bd75c5c3bff87f4bf37e429f0fb5631412ac9e
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-25 20:45:10 +0000

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,5 +1,7 @@
 #include "holberton.h"
 
+int getLength(unsigned int n);
+
 /**
  * get_bit - function that returns the value of a bit at a given index
  * @n: number passed
@@ -8,7 +10,14 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int count;
+	unsigned int count, length;
+
+	length = getLength(n);
+
+	if (index > (length - 1))
+	{
+		return (0);
+	}
 
 	if (index > sizeof(unsigned long int))
 	{
@@ -21,4 +30,20 @@ int get_bit(unsigned long int n, unsigned int index)
 	}
 
 	return (n & 1);
+}
+/**
+ * getLength - function prints out recursion
+ * @number: unsigned int
+ * Return: length
+ */
+int getLength(unsigned int number)
+{
+	int count = 1;
+
+	if ((number / 2) > 0)
+	{
+		count += getLength(number / 2);
+	}
+
+	return (count);
 }
