5699975ce1efdbf32df32546fd605f5e1bcaca3e
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-25 17:37:37 +0000

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,13 +6,16 @@
  * Return: the converter number or 0 if b is NULL or if non index element
  *
  */
+
+int _atoi(const char *s);
+
 unsigned int binary_to_uint(const char *b)
 {
-	int binary, lastNum , uInt = 0, count = 0;
+	int binary, lastNum, uInt = 0, count = 0;
 
-	binary = atoi(b);
+	binary = _atoi(b);
 
-	while(binary != 0)
+	while (binary != 0)
 	{
 		lastNum = binary % 10;
 		uInt += lastNum << count;
@@ -21,4 +24,29 @@ unsigned int binary_to_uint(const char *b)
 	}
 
 	return (uInt);
+}
+
+int _atoi(const char *s)
+{
+	int i = 0, len = 0, num = 0, sign = 1, ok = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	while (i < len && ok == 0)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = num * 10 + sign * (s[i] - 48);
+			if (s[i + 1] >= '0' && s[i + 1] <= '9')
+				ok = 0;
+			else
+				ok = 1;
+		}
+		i++;
+	}
+	return (num);
 }
