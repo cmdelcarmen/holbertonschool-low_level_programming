c005bea15c5ab9d635e4107fe6c8aed40132ab9a
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-26 00:28:44 +0000

    update

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 
 /**
- * set_bit - function that returns the value of a bit at a given index
+ * clear_bit - function that returns the value of a bit at a given index
  * @n: number passed
  * @index: index to count until
  * Return: 1 or -1
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 
-	unsigned long int newNum ;
+	unsigned long int newNum;
 
 	if (index > sizeof(unsigned long int) * 8)
 	{
-		return (-1);   
+		return (-1);
 	}
 
 	newNum = 1 << index;
