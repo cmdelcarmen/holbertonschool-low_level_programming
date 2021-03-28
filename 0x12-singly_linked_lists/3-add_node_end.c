310a4a4b3b49c74718025d66f5f07cabc9e32e0a
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-28 01:30:41 +0000

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,46 +1,94 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- *  * main - check the code for Holberton School students.
- *   *
- *    * Return: Always 0.
- *     */
-int main(void)
+ * duplicateString - function duplicates string
+ * @str: string passed
+ * Return: duplicated string
+ */
+char *duplicateString(const char *str)
 {
-	list_t *head;
+	char *copiedString;
+	int count = 0, arrayLength = 0;
 
-	head = NULL;
-	add_node_end(&head, "Anne");
-	add_node_end(&head, "Colton");
-	add_node_end(&head, "Corbin");
-	add_node_end(&head, "Daniel");
-	add_node_end(&head, "Danton");
-	add_node_end(&head, "David");
-	add_node_end(&head, "Gary");
-	add_node_end(&head, "Holden");
-	add_node_end(&head, "Ian");
-	add_node_end(&head, "Ian");
-	add_node_end(&head, "Jay");
-	add_node_end(&head, "Jennie");
-	add_node_end(&head, "Jimmy");
-	add_node_end(&head, "Justin");
-	add_node_end(&head, "Kalson");
-	add_node_end(&head, "Kina");
-	add_node_end(&head, "Matthew");
-	add_node_end(&head, "Max");
-	add_node_end(&head, "Michael");
-	add_node_end(&head, "Ntuj");
-	add_node_end(&head, "Philip");
-	add_node_end(&head, "Richard");
-	add_node_end(&head, "Samantha");
-	add_node_end(&head, "Stuart");
-	add_node_end(&head, "Swati");
-	add_node_end(&head, "Timothy");
-	add_node_end(&head, "Victor");
-	add_node_end(&head, "Walton");
-	print_list(head);
-	return (0);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (count = 0; str[count] != '\0'; count++)
+	{
+		arrayLength++;
+	}
+
+	copiedString = malloc(sizeof(char) * (arrayLength + 1));
+
+	if (copiedString == NULL)
+	{
+		return (NULL);
+	}
+
+	for (count = 0; str[count] != '\0'; count++)
+	{
+		copiedString[count] = str[count];
+	}
+	copiedString[count] = '\0';
+
+	return (copiedString);
+}
+
+/**
+ * getLength - function gets length of string
+ * @s: string passed
+ * Return: length of array
+ */
+int getLength(const char *s)
+{
+	int count, arrayLength = 0;
+
+	for (count = 0; s[count] != '\0'; count++)
+	{
+		arrayLength++;
+	}
+
+	return (arrayLength);
+}
+
+/**
+ * add_node_end - function adds node at the end of a list_t list
+ * @head: pointer to head of node
+ * @str: string that new node will contain
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *copy;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (*head != NULL)
+	{
+		*head = (*head)->next;
+	}
+
+	printf("finished while lopp\n");
+
+	copy = (list_t *)malloc(sizeof(list_t));
+		if (copy == NULL)
+		{
+			return (NULL);
+		}
+	printf("arrive 1\n");
+
+	copy->str = duplicateString(str);
+	copy->len = getLength(str);
+	copy->next = NULL;
+
+	printf("%s %d\n", copy->str, copy->len);
+
+	(*head)->next = copy;
+
+	printf("arrive 3\n");
+	return (copy);
 }
