5771fd88761b8e52a698bb3b99bb42edefaa7040
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-28 15:13:41 +0000

    update

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,14 +10,12 @@ void free_listint2(listint_t **head)
 
 	temp = *head;
 
-	if (*head == NULL)
-	{
-		free(*head);
-	}
 	while (*head != NULL)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
+
+	free(*head);
 }
