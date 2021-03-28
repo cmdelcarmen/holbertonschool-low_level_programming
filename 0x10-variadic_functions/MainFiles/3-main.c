ba708704cda3218b1aa7af8547ecedbba870bae1
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-28 05:44:09 +0000

    update

--- 0x10-variadic_functions/MainFiles/3-main.c
+++ 0x10-variadic_functions/MainFiles/3-main.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdio.h>
 
 /**
  *  * main - check the code for Holberton School students.
@@ -7,6 +8,9 @@
  *     */
 int main(void)
 {
+	print_all(NULL, NULL);
+	print_all("s",NULL);
+	print_all("eceeeisfp", 'H', 0, "lberton", 3.14);
 	print_all("ceis", 'H', 0, "lberton");
 	return (0);
 }
