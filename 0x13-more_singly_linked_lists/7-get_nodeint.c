3c4a51e3aed65b5ec0ea23a704abc028ea3fbd37
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-29 22:24:19 +0000

    update

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,6 +10,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	while (head != NULL)
 	{
 		if (count == index)
