f911f45cf6a492c6df9c61bab23116230251486d
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-29 22:18:52 +0000

    update

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,10 +8,16 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
+	int n;
 
+	if (head == NULL)
+	{
+		return (0);
+	}
+	n = (*head)->n;
 	temp = (*head)->next;
 	free(*head);
 	*head = temp;
 
-	return ((*head)->n);
+	return (n);
 }
