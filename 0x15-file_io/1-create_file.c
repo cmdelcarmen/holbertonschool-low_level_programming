016f5205842bdbf385fac0d833c7e37335b7caa6
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-30 21:41:36 +0000

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -33,10 +33,20 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_CREAT, 0600);
+		if (fd == -1)
+		{
+			return (-1);
+		}
+		close(fd);
+		return (1);
+	}
 
 	lengthOfString = _strlen(text_content);
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 	{
 		return (-1);
