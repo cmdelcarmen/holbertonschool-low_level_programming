a917079b2e54c6e9e01f01992b7803bf817ae702
cmdelcarmen <delcarmencaroline@gmail.com>
2021-03-31 03:56:38 +0000

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,22 +10,21 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, numOfLettersRead;
-
 	char *buffer;
+
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+	{
+		return (0);
+	}
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
 
-	buffer = malloc(sizeof(char) * letters);
-			if (buffer == NULL)
-			{
-				return (0);
-			}
-
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 	{
 		return (0);
