3b27e14d200fb86c635e682021fb25367d40e25e
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-28 17:58:22 +0000

    added casting

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - function add an element to the hash table
+ * @ht: the hash table you want to add or update the key/value to
+ * @key: the key, cannot be an empty string
+ * @value: the value associated with the key, value must be duplicated,
+ * value can be an empty string
+ * Return: 1 on sucess, 0 on fail
+ *
+ * Incase of collision, adds the new node at the beginning of the list
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *newNode = NULL;
+	unsigned long int index = 0;
+
+	newNode = malloc(sizeof(hash_node_t));
+		if (newNode == NULL)
+			return (0);
+
+	newNode->key = (char *)key;
+	newNode->value = (char *)value;
+	index = key_index((const unsigned char *)key, ht->size);
+
+	ht->array[index] = newNode;
+
+	return (0);
+}
