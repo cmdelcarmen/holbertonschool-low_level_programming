bef0f35f51456bf6636132a03c6c2babc70e89b7
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-28 23:38:14 +0000

    ignore

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int index = 0;
+	hash_node_t node_pointer;
+
+	while(ht->array[index])
+	{
+		if (ht->array[index])
+		{
+			while (ht->array[index])
+			{
+				node_pointer = ht->array[index]->next;
+				free(ht->array[index]);
+				ht->array[index] = node_pointer;
+			}
+		}
+		else
+			free(ht->array[index]);
+
+		index++;
+	}
+	free(ht->array);
+	free(ht);
+}
