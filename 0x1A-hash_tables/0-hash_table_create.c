e3f45a8344206fe84e2cdba1912d9b1a00fbc8aa
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-28 16:11:27 +0000

    fixed amount of mem allocated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,15 +13,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 		if (table == NULL)
 			return NULL;
 
-	table->array = malloc(sizeof(hash_node_t *) * (size + 1));
+	table->array = malloc(sizeof(hash_node_t *) * size);
 		if (table->array == NULL)
 			return NULL;
 
 	for(idx = 0; idx < size; idx++)
-	{
 		table->array[idx] = NULL;
-	}
-	table->array[idx] = NULL;
 
 	return (table);
 }
