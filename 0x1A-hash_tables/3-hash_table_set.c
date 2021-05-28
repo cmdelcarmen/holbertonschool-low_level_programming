f515fb5c44d1991296c3241ae52085a22216d2de
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-28 18:04:50 +0000

    added strdup

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,7 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (0);
 
 	newNode->key = (char *)key;
-	newNode->value = (char *)value;
+	newNode->value = strdup((char *)value);
 	index = key_index((const unsigned char *)key, ht->size);
 
 	ht->array[index] = newNode;
