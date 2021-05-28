27db77a6d410db19a7bd666fdcad667c33c59af6
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-28 18:47:38 +0000

    added case for empty string

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,6 +19,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *newNode = NULL;
 	unsigned long int index = 0;
 
+	if (strcmp(key, "") == 0)
+		return (0);
+
 	newNode = malloc(sizeof(hash_node_t));
 		if (newNode == NULL)
 			return (0);
