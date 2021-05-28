fe6eae3fea36944a8f7b2a2e49dde97c564e6bbe
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-28 20:42:52 +0000

    changed while loop

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,16 +13,14 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (ht == NULL || key == NULL)
 		return (NULL);
 
-	while (index < (ht->size))
+	index = key_index((const unsigned char *)key, ht->size);
+
+	while (ht->array[index])
 	{
-		if (ht->array[index] != NULL)
-		{
-			if (strcmp((char *)(ht->array[index]->key), (char *)key) == 0)
-			{
-				return (ht->array[index]->value);
-			}
-		}
-		index++;
+		if (strcmp((char *)(ht->array[index]->key), (char *)key) == 0)
+			return (ht->array[index]->value);
+
+		ht->array[index]++;
 	}
 	return (NULL);
 }
