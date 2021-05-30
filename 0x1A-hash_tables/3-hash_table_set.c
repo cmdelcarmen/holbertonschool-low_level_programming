844ea00dec7fc4fdced0736c59146e509d1a46d8
cmdelcarmen <delcarmencaroline@gmail.com>
2021-05-30 16:42:39 +0000

    update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,7 +34,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		while(ht->array[index])
 		{
-			printf("inside while loop\n");
 			ht->array[index] = ht->array[index]->next;
 		}
 	}
