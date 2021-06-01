2576686731fc126eaeb5373f0430c61c3d7fa860
cmdelcarmen <delcarmencaroline@gmail.com>
2021-06-01 01:23:27 +0000

    update

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,31 +9,24 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned int index = 0;
 	int comma = 0;
 
-	if (ht == NULL)
+	printf("{");
+	while (index < ht->size)
 	{
-		return;
-	}
-	else
-	{
-		printf("{");
-		while (index < ht->size)
+		if (ht->array[index] != NULL)
 		{
-			if (ht->array[index] != NULL)
+			while (ht->array[index])
 			{
-				while (ht->array[index])
+				if (comma == 1)
 				{
-					if (comma == 1)
-					{
-						printf(", ");
-					}
-					printf("'%s': '%s'", ht->array[index]->key, ht->array[index]->value);
-					comma = 1;
-
-					ht->array[index] = ht->array[index]->next;
+					printf(", ");
 				}
+				printf("'%s': '%s'", ht->array[index]->key, ht->array[index]->value);
+				comma = 1;
+
+				ht->array[index] = ht->array[index]->next;
 			}
-			index++;
 		}
-		printf("}\n");
+		index++;
 	}
+	printf("}\n");
 }
