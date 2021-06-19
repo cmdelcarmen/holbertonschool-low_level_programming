f67d1891e89629aa5c2b4f217fef554c350257b2
cmdelcarmen <delcarmencaroline@gmail.com>
2021-06-19 15:31:42 +0000

    got rid of unnecessary variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,12 @@
 */
 int main(void)
 {
-	char char_var;
-	int int_var;
-	long int long_int_var;
-	long long int long_long_int;
-	float float_var;
 
-	printf("Size of a char: %zu byte(s)\n", sizeof(char_var));
-	printf("Size of an int: %zu byte(s)\n", sizeof(int_var));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long_int_var));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long_long_int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float_var));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
 }
